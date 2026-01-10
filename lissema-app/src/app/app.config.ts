1ffb749d50235b27d8ce1b4dd9e1dc08b070160e
ELIAS2401 <tuccieliasluis@gmail.com>
2026-01-10 15:11:26 -0300

    Update app.config.ts

--- lissema-app/src/app/app.config.ts
+++ lissema-app/src/app/app.config.ts
@@ -9,7 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withHashLocation()),
-    provideHttpClient(),
     provideHttpClient(
       withInterceptors([authInterceptor])
     )
