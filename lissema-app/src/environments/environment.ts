7af020d5cd328f4d7b7dc3e2e4ecae312935cc8c
ELIAS2401 <tuccieliasluis@gmail.com>
2026-01-31 22:55:09 -0300

    Update environment.ts

--- lissema-app/src/environments/environment.ts
+++ lissema-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://localhost:3000'
-    apiUrl: 'https://apilisemma-production.up.railway.app/'
+    apiUrl: 'https://apilisemma-production.up.railway.app'
 };
